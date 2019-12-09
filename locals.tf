2d1eba7e6dab14573355bca53e8e16d649e6cc23
wwalpha <wwalpha@gmail.com>
2019-12-09 18:22:27 +0900

    bug fix

--- locals.tf
+++ locals.tf
@@ -12,7 +12,7 @@ locals {
   vpc_config                  = var.subnet_ids != null ? map("subnet_ids", var.subnet_ids, "security_group_ids", var.security_group_ids) : local.empty_map
   routing_config              = var.additional_version_weights != null ? list(var.additional_version_weights) : local.empty_array
   filename                    = var.dummy_enabled ? "${path.module}/index.zip" : var.source_output_path
-  source_code_hash            = var.dummy_enabled ? filebase64sha256("${path.module}/index.zip") : filebase64sha256(var.source_output_path)
+  source_code_hash            = var.dummy_enabled ? filebase64sha256("${path.module}/index.zip") : filebase64sha256(data.archive_file.this.output_path)
   aws_lambda_function         = var.dummy_enabled ? aws_lambda_function.dummy[0] : aws_lambda_function.this[0]
   aws_xray_write_only_arn     = "arn:aws:iam::aws:policy/AWSXrayWriteOnlyAccess"
   invoke_arn                  = var.alias_name != null ? aws_lambda_alias.this[0].invoke_arn : local.aws_lambda_function.invoke_arn
